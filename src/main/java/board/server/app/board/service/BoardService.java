e6c1bae7ae56ffc7c56f8e3d1cb033e8a26ed82a
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2024-07-19 19:41:59 +0900

    Refactor: BoardService - 선언 클래스명 변경

--- src/main/java/board/server/app/board/service/BoardService.java
+++ src/main/java/board/server/app/board/service/BoardService.java
@@ -2,9 +2,11 @@ package board.server.app.board.service;
 
 
 import board.server.app.board.entity.Board;
+import board.server.app.board.repository.BoardRepository;
 import board.server.app.board.repository.JdbcTemplateBoardRepository;
 import board.server.app.member.entity.Member;
 import board.server.app.member.repository.JdbcTemplateMemberRepository;
+import board.server.app.member.repository.MemberRepository;
 import board.server.error.errorcode.CustomExceptionCode;
 import board.server.error.exception.BusinessLogicException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +19,20 @@ import java.util.List;
 @Service
 @Transactional
 public class BoardService {
-    private final JdbcTemplateBoardRepository jdbcTemplateBoardRepository;
-    private final JdbcTemplateMemberRepository jdbcTemplateMemberRepository;
+    private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public BoardService(JdbcTemplateBoardRepository jdbcTemplateBoardRepository, JdbcTemplateMemberRepository jdbcTemplateMemberRepository) {
-        this.jdbcTemplateBoardRepository = jdbcTemplateBoardRepository;
-        this.jdbcTemplateMemberRepository = jdbcTemplateMemberRepository;
+    public BoardService(JdbcTemplateBoardRepository jdbcTemplateBoardRepository, JdbcTemplateMemberRepository memberRepository) {
+        this.boardRepository = jdbcTemplateBoardRepository;
+        this.memberRepository = memberRepository;
     }
 
 
     public Board getBoard(Long id, String name){
         validatePresentMember(name);
 
-        return jdbcTemplateBoardRepository.findByIdAndName(id, name).orElseThrow(() ->
+        return boardRepository.findByIdAndName(id, name).orElseThrow(() ->
             new BusinessLogicException(CustomExceptionCode.BOARD_NOT_FOUND)
         );
     }
@@ -38,34 +40,34 @@ public class BoardService {
     public List<Board> getBoardList(String name){
         Long author = validatePresentMember(name).getId();
 
-        return jdbcTemplateBoardRepository.findByAuthor(author);
+        return boardRepository.findByAuthor(author);
     }
 
     public List<Board> getBoardListAll(){
-        return jdbcTemplateBoardRepository.findAll();
+        return boardRepository.findAll();
     }
 
     public Long join(Board board, String name){
 //        validatePresentMember(board.getAuthor());
         validatePresentMember(board.getAuthor(), name);
 
-        return jdbcTemplateBoardRepository.save(board).getId();
+        return boardRepository.save(board).getId();
     }
 
     private void validatePresentMember(Long id, String name) {
-        jdbcTemplateMemberRepository.findByIdAndName(id, name).orElseThrow(() ->
+        memberRepository.findByIdAndName(id, name).orElseThrow(() ->
                 new BusinessLogicException(CustomExceptionCode.MEMBER_NO_PERMISSION)
         );
     }
 
     // POST, GET - 글 작성 전 or 읽어오기 전, 유효한 계정인지 체크 (Integrity Consrtrait)
     private void validatePresentMember(Long author) {
-        jdbcTemplateMemberRepository.findById(author).orElseThrow(() ->
+        memberRepository.findById(author).orElseThrow(() ->
                 new BusinessLogicException(CustomExceptionCode.MEMBER_NO_PERMISSION)
         );
     }
     private Member validatePresentMember(String name) {
-        return jdbcTemplateMemberRepository.findByName(name).orElseThrow(() ->
+        return memberRepository.findByName(name).orElseThrow(() ->
                 new BusinessLogicException(CustomExceptionCode.MEMBER_NO_PERMISSION)
         );
     }
