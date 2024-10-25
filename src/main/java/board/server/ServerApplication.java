6aee2823a2120e2bb05fad0ba26257fd60671bd3
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2024-10-25 16:56:37 +0900

    Fix: server 시간대 변경

--- src/main/java/board/server/ServerApplication.java
+++ src/main/java/board/server/ServerApplication.java
@@ -3,10 +3,13 @@ package board.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
