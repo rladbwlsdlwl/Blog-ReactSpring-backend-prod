1ba78a0dc77af11bac502119694a7076098d962a
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2024-12-06 18:46:36 +0900

    Fix: File - JPA 레포지토리 빈 등록

--- src/main/java/board/server/app/file/repository/JpaFileRepository.java
+++ src/main/java/board/server/app/file/repository/JpaFileRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-//@Repository
+@Repository
 public class JpaFileRepository implements FileRepository{
 
     @Autowired
